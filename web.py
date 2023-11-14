a7bc0d73f3d9cbe29eb202614c4d399f5bc42812
s11016880 <b0979798210@gmail.com>
2023-11-14 14:05:19 +0800

    version 0: html5

--- web.py
+++ web.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	homepage = "<h1>顏秉庭Python網頁</h1>"
-	homepage += "<a href=/mis>MIS</a><br>"
-	homepage += "<a href=/today>顯示日期時間</a><br>"
-	homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
-	homepage += "<a href=/about>秉庭簡介網頁</a><br>"
-	homepage += "<a href=/account>網頁輸入帳密</a><br>"
+	homepage += "<h1>楊承恩職涯網頁</h1>"
+	homepage += "<a href=/相關工作介紹></a><br>"
+	homepage += "<a href=/welcome?nick=tcyang>職崖測驗結果</a><br>"
+	homepage += "<a href=/about>求職履歷自傳</a><br>"
 	return homepage
 
 @app.route("/mis")
